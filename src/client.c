948152bc6bdf82c708533562c12489d3b653e04b
tumerlvn <tamer.makhmutov@gmail.com>
2020-12-10 23:10:49 +0600

    version 1 and test

--- src/client.c
+++ src/client.c
@@ -18,6 +18,137 @@ enum errors {
     ERR_CONNECT
 };
 
+int init_socket(const char *ip, int port);
+char *getWord();
+int getIpAndPort(char *request, char **inIp, int *inPort);
+int send_request(int server, char* word);
+char* request_path(char* request, int index);
+
+
+int main(int argc, char **argv) {
+    puts("Write data:");
+    char ch;
+    int server = -1;
+    int index;
+    char *ip = NULL;
+    int port = 0;   
+
+    char *request = getWord(); 
+    while (1) {
+        index = getIpAndPort(request, &ip, &port);
+        if (index != -1) {
+            puts(ip);
+            printf("%d\n%d\n", port, index);
+
+            close(server);
+            server = init_socket(ip, port);
+       
+            if (fork() == 0) {
+                int j = 0;
+                while (read(server, &ch, 1) > 0) {
+                    write(1, &ch, 1);
+                }
+                close(server);
+                exit(0);
+            }
+            char *word = request_path(request, index);
+            puts(word);
+            send_request(server, word);
+            free(word);
+            free(ip);
+        }
+        free(request);
+        request = getWord();
+
+    }
+    free(request); 
+    return OK;
+}
+
+char* request_path(char* request, int index) {
+    int j = index;
+   
+    char *word = NULL;
+    int size = 0;
+    while (request[j] != '\0' && request[j] != ' ') {
+        size++;
+        word = realloc(word, size * sizeof(char));
+        word[size-1] = request[j];
+        j++;
+    }
+    size++;
+    word = realloc(word, size * sizeof(char));
+    word[size-1] = '\0';
+    return word;
+}
+
+int send_request(int server, char* word) {
+    char h1[] = "GET /";
+    char h2[] = " HTTP/1.1\r\nHost: mymath.info\r\n\r\n";
+    write(server, h1, strlen(h1));
+    write(server, word, strlen(word)); 
+    write(server, h2, strlen(h2));
+}
+
+char *getWord() {
+    char *word = NULL;
+    char ch;
+    int size = 0;
+    ch = getchar();
+    while (ch != '\n' && ch != ' ') {
+        size++;
+        word = realloc(word, size * sizeof(char));
+        word[size-1] = ch;
+        ch = getchar();
+    }
+    size++;
+    word = realloc(word, size * sizeof(char));
+    word[size-1] = '\0';
+    return word;
+}
+
+int getIpAndPort(char *request, char **inIp, int *inPort) { //returns index of '/' + 1;
+    int i = 0;
+    int i1 = -1, i2 = -1;
+    while (request[i] != '\0') {
+        if (request[i] == ':' && i1 < 0) {
+            i1 = i;
+        } else if (request[i] == '/' && i2 < 0) {
+            i2 = i;
+        }
+        i++;
+    }
+    if (i1 <= 0 || i2 <= 0 || (i2 - i1) <= 1) {
+        return -1;
+    } else {
+        i = 0;
+        char *ip = NULL;
+        int port = 0;
+        while (request[i] != ':') {
+            i++;
+            ip = realloc(ip, i * sizeof(char)); 
+            ip[i-1] = request[i-1];
+        }
+        i++;
+        ip = realloc(ip, i * sizeof(char));
+        ip[i-1] = '\0';
+        while (request[i] != '/') {
+            port *= 10;
+            port += request[i] - '0';
+            i++;
+        }
+        i++;
+        if (i == strlen(request)) {
+            free(ip);
+            return -1;
+        } else {
+            *inIp = ip;
+            *inPort = port;
+            return i;
+        }
+    }
+}
+
 int init_socket(const char *ip, int port) {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct hostent *host = gethostbyname(ip);
@@ -42,45 +173,4 @@ int init_socket(const char *ip, int port) {
         exit(ERR_CONNECT);
     }
     return server_socket;
-}
-
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        puts("Incorrect args.");
-        puts("./client <ip> <port>");
-        puts("Example:");
-        puts("./client 127.0.0.1 5000");
-        return ERR_INCORRECT_ARGS;
-    }
-    char *ip = argv[1];
-    int port = atoi(argv[2]);
-    int server = init_socket(ip, port);
-
-    char ch;
-    
-    puts("Write data:");
-    if (fork() == 0) {
-        int j = 0;
-        while (read(server, &ch, 1) >= 0) {
-            write(1, &ch, 1);
-        }
-        exit(0);
-    }
-    char word[100] = {0};
-    int j = 0;
-    while (read(0, &(word[j]), 1) >= 0) {
-        j++;
-        if (word[j-1] == '\n' || word[j-1] == ' ') {
-            char h1[] = "GET ";
-            char h2[] = " HTTP/1.1\nHost: mymath.info\n\n";
-            write(server, h1, strlen(h1));
-            write(server, word, j-1); 
-            write(server, h2, strlen(h2));
-            j = 0;
-        }
-    }
-
-    wait(NULL);
-    close(server);
-    return OK;
 }
--- src/server.c
+++ src/server.c
@@ -23,6 +23,226 @@ enum errors {
     ERR_LISTEN
 };
 
+typedef struct {
+    char *ext;
+    char *mediatype;
+} extn;
+
+//Possible media types
+extn addition[] = {
+    {"gif", "image/gif" },
+    {"txt", "text/html" },//text/plain
+    {"jpg", "image/jpg" },
+    {"jpeg","image/jpeg"},
+    {"png", "image/png" },
+    {"ico", "image/ico" },
+    {"zip", "image/zip" },
+    {"gz",  "image/gz"  },
+    {"tar", "image/tar" },
+    {"htm", "text/html" },
+    {"html","text/html" },
+    {"php", "text/html" },
+    {"pdf","application/pdf"},
+    {"zip","application/octet-stream"},
+    {"rar","application/octet-stream"},
+    {0,0} 
+};
+
+int init_socket(int port);
+int interaction_client(int client_socket);
+char* get_request(int client_socket);
+char* get_path(char* request_text);
+void send_to_client(int client_socket, char* request_path, char* ext);
+void c200(int client_socket, int fd, int index_extension);
+void c404(int client_socket);
+int run_binary(int client_socket, char* path);
+int file_exists(char* path);
+char* create_buf(int fd);
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        puts("Incorrect args.");
+        puts("./server <port> <clients>");
+        puts("Example:");
+        puts("./server 5000 2");
+        return ERR_INCORRECT_ARGS;
+    }
+    int port = atoi(argv[1]);
+    int server_socket = init_socket(port);
+    puts("Wait for connection");
+    struct sockaddr_in client_address;
+    socklen_t size = sizeof(client_address);
+    int clients = atoi(argv[2]);
+    int *client_socket = malloc(clients * sizeof(int));
+
+    for (int i = 0; i < clients; i++) {
+        client_socket[i] = accept(server_socket, 
+                           (struct sockaddr *) &client_address,
+                           &size);
+        printf("Client-#%d connected\n", i);
+        if (fork() == 0) {
+            interaction_client(client_socket[i]);
+            exit(0);
+        }
+        close(client_socket[i]);
+    }
+
+    for (int i = 0; i < clients; i++) {
+        wait(NULL);
+    }
+
+    free(client_socket);
+    return OK;
+}
+
+int interaction_client(int client_socket) {
+    while(1) {
+        char *request_text = get_request(client_socket);
+        if (request_text && strlen(request_text)) {
+            puts(request_text);
+            char *request_path = get_path(request_text);
+            char *ext = strchr(request_path, '.');
+            if (!file_exists(request_path)) {
+                c404(client_socket); 
+            } else if (ext != NULL) {
+                send_to_client(client_socket, request_path, ext + 1); 
+            } else {
+                run_binary(client_socket, request_path);
+            }
+            free(request_text);
+        } else {
+            free(request_text);
+            break;
+        }
+    }
+    puts("break");
+    return 0;
+}
+
+char* create_buf(int fd) {
+    char* buf = NULL;
+    int size = 0;
+    char ch;
+    while (read(fd, &ch, 1) > 0) {
+        size++;
+        buf = realloc(buf, size * sizeof(char));
+        buf[size-1] = ch;
+    }
+    size++;
+    buf = realloc(buf, size * sizeof(char));
+    buf[size-1] = '\0';
+    return buf;
+}
+
+int file_exists (char* name) {
+    return (access(name + 1, F_OK) != -1);
+}
+
+int run_binary(int client_socket, char* path) {//int run_binary(path, query_string)
+    int pid1 = fork(); 
+    if (pid1 == 0) {
+        int fd[2];
+        pipe(fd);
+        int pid = fork();
+
+        if (pid == 0) {
+            dup2(fd[1], 1);
+            close(fd[0]);
+            close(fd[1]);
+            execv(path + 1, NULL);
+            exit(0);
+        }
+        close(fd[1]);
+        c200(client_socket, fd[0], 1);
+        close(fd[0]);
+        waitpid(pid, NULL, 0);
+    }
+    waitpid(pid1, NULL, 0);
+    return 0;
+}
+
+void send_to_client(int client_socket, char* request_path, char* ext) {
+    int i = 0;
+    while (addition[i].ext) {
+        if (strcmp(addition[i].ext, ext) != 0) {
+            i++;
+        } else {
+            break;
+        }
+    }
+    if (!addition[i].ext) {
+        puts("NOT FOUND");
+        c404(client_socket);
+        return;
+    }
+    int fd =  open(request_path + 1, O_RDONLY);
+    puts(request_path + 1);
+    if (fd < 0) {
+        puts("NOT FOUND");
+        c404(client_socket);
+        return;
+    } else {
+        c200(client_socket, fd, i);
+    }
+
+    close(fd);
+}
+
+void c200(int client_socket, int fd, int index_extension) {
+    char h1[] = "HTTP/1.1 200\r\ncontent-type: %s\r\ncontent-length: %d\r\n\r\n";
+    char h2[] = "<!DOCTYPE html><html><h1>";
+    char h3[] = "</h1></html>";
+    char msg[100];
+    char* buf = create_buf(fd);
+
+    sprintf(msg, h1, addition[index_extension].mediatype, strlen(h2) + strlen(buf) + strlen(h3));
+    write(client_socket, msg, strlen(msg));
+    write(client_socket, h2, strlen(h2));
+    write(client_socket, buf, strlen(buf));
+    write(client_socket, h3, strlen(h3));
+    free(buf);
+}
+
+void c404(int client_socket) {
+    char h1[] = "HTTP/1.1 404\r\ncontent-type: text/html\r\ncontent-length: %d\r\n\r\n";
+    char buf[100];
+    char h2[] = "<!DOCTYPE html><html><h1>Not Found</h1></html>";
+    sprintf(buf, h1, strlen(h2));
+    write(client_socket, buf, strlen(buf));
+    write(client_socket, h2, strlen(h2));
+}
+
+char* get_path(char* request_text) {
+    char *req;
+    req = strtok(request_text, " \r\n");
+    req = strtok(NULL, " \r\n");
+    puts(req);
+    return req;
+}
+
+char* get_request(int client_socket) {
+    char *request_text = NULL;
+    int size = 0;
+    char ch, prev = '\0';
+    while (read(client_socket, &ch, 1) > 0) {
+        size++;
+        request_text = realloc(request_text, size * sizeof(char));
+        request_text[size-1] = ch;
+        if (prev == '\n' && ch == '\r') {
+            read(client_socket, &ch, 1);
+            size++;
+            request_text = realloc(request_text, size * sizeof(char));
+            request_text[size-1] = ch;
+            break;
+        }
+        prev = ch;
+    }
+    size++;
+    request_text = realloc(request_text, size * sizeof(char));
+    request_text[size-1] = '\0';
+    return request_text;
+}
+
 int init_socket(int port) {
     int server_socket, socket_option = 1;
     struct sockaddr_in server_address;
@@ -56,110 +276,4 @@ int init_socket(int port) {
         exit(ERR_LISTEN);
     }
     return server_socket;
-}
-
-int getFname(char *fname, int client_socket) {
-    int size = 0;
-    char http[28];
-    while(read(client_socket, &(fname[size]), 1) > 0) {
-        size++;
-        if (fname[size-1] == ' ') {
-            fname[size-1] = 0;
-            break;
-        }
-    }
-    read(client_socket, http, 28);
-    return size;
-}
-//
-void writeNum(int client_socket, int size) {
-    char buf[1000] = {0};
-    char bufSize = 0;
-    int tmp = size;
-    while (tmp > 0) {
-        bufSize++;
-        tmp /= 10;
-    }
-    if (size == 0) {
-        bufSize++;
-    }
-    snprintf(buf, bufSize + 1, "%d", size);
-    write(client_socket, buf, bufSize); 
-}
-
-void sendDataBack(char *fname, int client_socket) {
-    char ch;
-    int fd =  open(fname, O_RDONLY);
-    if (fd > 0) {
-        char h1[] = "HTTP/1.1 200\n";
-        char h2[] = "content-type: html/text\n";
-        char h3[] = "content-length: ";
-        write(client_socket, h1, strlen(h1));
-        write(client_socket, h2, strlen(h2));
-        write(client_socket, h3, strlen(h3));
-        int size = 0; 
-        char buf[10000] = {0};
-        while (read(fd, &ch, 1)) {
-            buf[size] = ch;
-            size++;
-        }
-        writeNum(client_socket, size);
-        write(client_socket, "\n\n", 2);
-        write(client_socket, buf, size);
-    } else {
-        char h[] = "HTTP/1.1 404\n";
-        write(client_socket, h, strlen(h));
-    }
-}
-
-void infoExchange (int client_socket) {
-    int j = 0;
-    char ch;
-    while (read(client_socket, &ch, 1) > 0) {
-        if (ch == ' ') {
-            char fname[100] = {0};
-            j = getFname(fname, client_socket);
-            if (fork() == 0) {
-                sendDataBack(fname, client_socket);
-                exit(0);
-            }
-            wait(NULL);
-            j = 0; 
-        }
-    }
-}
-
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        puts("Incorrect args.");
-        puts("./server <port> <clients>");
-        puts("Example:");
-        puts("./server 5000 2");
-        return ERR_INCORRECT_ARGS;
-    }
-    int port = atoi(argv[1]);
-    int server_socket = init_socket(port);
-    puts("Wait for connection");
-    struct sockaddr_in client_address;
-    socklen_t size = sizeof(client_address);
-    int clients = atoi(argv[2]);
-    int *client_socket = malloc(clients * sizeof(int));
-
-    for (int i = 0; i < clients; i++) {
-        if (fork() == 0) {
-            client_socket[i] = accept(server_socket, 
-                               (struct sockaddr *) &client_address,
-                               &size);
-            infoExchange(client_socket[i]);
-            close(client_socket[i]);
-            exit(0);
-        }
-    }
-
-    for (int i = 0; i < clients; i++) {
-        wait(NULL);
-    }
-
-    free(client_socket);
-    return OK;
 }
